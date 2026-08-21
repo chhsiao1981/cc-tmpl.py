e46a2a882a9a83675f990717b333a9b6a324ca61
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-08-21 08:21:20 -0400

    refactor: argparse: refactor ensure_command => ensure_dev_module. (#27)
    Signed-off-by: Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>

--- src/cc_tmpl_py/utils.py
+++ src/cc_tmpl_py/utils.py
@@ -1,9 +1,11 @@
+import argparse
 import os
 import sys
+from argparse import ArgumentParser
 from importlib import resources
 from pathlib import Path
 
-from .const import COMMANDS, PROJECT, logger
+from .const import PROJECT, logger
 
 
 def parse_tmpl_dir(tmpl_dir: str, module: str):
@@ -19,21 +21,52 @@ def parse_tmpl_dir(tmpl_dir: str, module: str):
     return path
 
 
-def ensure_command():
+def get_subcommands(parser: ArgumentParser) -> list[str]:
+    for action in parser._actions:
+        if isinstance(action, argparse._SubParsersAction):
+            return list(action.choices.keys())
+    return []
+
+
+def ensure_dev_module(known_commands: list[str], singleton_flags: list[str]):
+    '''
+    to deal with the args with only modules.
+
+    CONTEXT:
+
+    1. command is always the 1st positional argument.
+    2. we may have --dir dev, as dev is a parameter, not a command.
+    3. we may have dev init, as init is a parameter for dev, not a command.
+    4. only dev is with positional arg, as module.
+    5. for other commands: either singleton (init, docker), or optional args with --.
+
+    STEPS (as state machine):
+
+    is-to-skip
+    is-to-insert-idx
+
+    for loop the argv:
+        if is-skip: reset is-skip and skip. (as the parameter of '-', '--').
+        elif -h: return. (help, no need to do anything.)
+        elif singleton-flags: continue (singleton flags).
+        elif flags: set is-skip. (flags with 1 parameter).
+        elif known-commands: return. (command already specified.)
+        else: set is_to_insert_idx. (no command).
+    '''
     is_skip = False
     is_to_insert_idx = -1
     for idx, each_argv in enumerate(sys.argv[1:]):
         if is_skip:
             is_skip = False
             continue
-        if each_argv == '-h' or each_argv == '--help':
+        elif each_argv == '-h' or each_argv == '--help':
             return
-        elif each_argv == '-f' or each_argv == '--force':
+        elif each_argv in singleton_flags:
             continue
         elif each_argv.startswith('-'):
             is_skip = True
             continue
-        elif each_argv in COMMANDS:
+        elif each_argv in known_commands:
             return
         else:
             is_to_insert_idx = idx + 1
